ad46430fe51e4cab727903bba26d20ccb7803308
dianasaur323 <dianahsieh323@gmail.com>
2019-02-03 02:31:53 -0500

    Importing reddit data

--- supervised_learning/main.py
+++ supervised_learning/main.py
@@ -1,7 +1,12 @@
 import models
+import pandas as pd
 
 if __name__ == "__main__":
     model_name = input("Model name?: ")
+    data_set = input("Data set?: ")
+    if(data_set == 'reddit'):
+        df_test = pd.read_csv('reddit_data/reddit_200k_test.csv',encoding='ISO-8859-1')
+        df_train = pd.read_csv('reddit_data/reddit_200k_train.csv',encoding='ISO-8859-1')
     if(model_name.upper() == "DECISION TREE"):
         model = models.DecisionTree()
-        print("Decision Tree")
+        print("decision tree")
